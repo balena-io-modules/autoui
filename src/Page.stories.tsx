9fcda189362ec16ff37181b75ab49acd3cbdd2ff
Stevche Radevski <sradevski@live.com>
2020-11-24 13:37:33 +0100

    Add initial repo setup
    Change-type: minor
    Signed-off-by: Stevche Radevski <stevche@balena.io>

---
+++ src/Page.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Page } from "./Page";
+
+export default {
+  title: "Example/Page",
+  component: Page,
+};
+
+const Template = (args) => <Page {...args} />;
+export const Default = Template.bind({});
