cf2d456d96c03d46c6004c3f44604d571a5dcca9
Andrea Rosci <jsreds@Andreas-MacBook-Pro.local>
2024-08-12 22:19:18 +0200

    Add Widget and Renderer component
    Change-type: minor

---
+++ src/components/Widget/Formats/DisabledTextWidget.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTranslation } from '../../../hooks/useTranslation';
+import { JsonTypes, widgetFactory } from '../utils';
+import { Truncate } from '@balena/ui-shared-components';
+
+export const DisabledTextWidget = widgetFactory('DisabledText', {}, [
+	JsonTypes.string,
+	JsonTypes.number,
+	JsonTypes.null,
+])(({ value }) => {
+	const { t } = useTranslation();
+	const val =
+		value != null && typeof value !== 'string' ? value.toString() : value;
+	return (
+		// TODO: check which color do we use for disabled text
+		<Truncate sx={{ maxWidth: '350px' }} color="#b3b6b9" lineCamp={1}>
+			{val ?? t('info.not_defined')}
+		</Truncate>
+	);
+});
